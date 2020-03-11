97ecf29c446aa18518e1265732b46ca9b5acd158
Sarah Hart Landolt <sarahhartlandolt@Sarahs-MacBook-Pro.local>
2020-03-11 13:26:31 -0500

    added eventhub event listeners according to directions

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -1,21 +1,46 @@
 import { useConvictions } from "./ConvictionProvider.js"
 
+/*
+    Which element in your HTML contains all components?
+    That's your Event Hub. Get a reference to it here.
+*/
+const eventHub = document.querySelector(".container")
 const contentTarget = document.querySelector(".filters__crime")
 
-const ConvictionSelect = () => {
-    const convictions = useConvictions()
+// On the content target, listen for a "change" event.
+contentTarget.addEventListener("change", changeEvent => {
 
-    const render = convictionsCollection => {
-
-        contentTarget.innerHTML = `
-            <select class="dropdown" id="crimeSelect">
-                <option value="0">Please select a crime...</option>
-                ${convictionsCollection.map(crime => `<option> ${crime.name}</option>`) }
-            </select>
-        `
+    // Only do this if the `crimeSelect` element was changed
+    if (changeEvent.target.id.startsWith("crimeSelect")) {
+        const selectedCrime = changeEvent.target.value 
+        // Create custom event. Provide an appropriate name.
+        const crimeChosenEvent = new CustomEvent("crimeChosen", {
+            detail: {
+                crimeName: selectedCrime
+            }
+        })
+        // Dispatch to event hub
+        eventHub.dispatchEvent(crimeChosenEvent)
     }
+})
 
-    render(convictions)
+
+const render = convictionsCollection => {
+    contentTarget.innerHTML = `
+        <select class="dropdown" id="crimeSelect">
+            <option value="0">Please select a crime...</option>   
+        ${
+            convictionsCollection.map(singleConviction => {
+                return `<option>${singleConviction.name}</option>`
+            })
+        }
+ 
+        </select>
+    `
 }
 
-export default ConvictionSelect
+
+export const ConvictionSelect = () => {
+    const convictions = useConvictions()
+    render(convictions)
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 import { getOfficers } from "./officers/OfficerProvider.js";
 import { getCriminals } from "./criminals/CriminalProvider.js";
-import CriminalList from "./criminals/CriminalList.js";
-import ConvictionSelect from "./convictions/ConvictionSelect.js";
+import {CriminalList} from "./criminals/CriminalList.js";
+import {ConvictionSelect} from "./convictions/ConvictionSelect.js";
 import { getConvictions } from "./convictions/ConvictionProvider.js";
 
 
