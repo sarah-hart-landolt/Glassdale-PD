25b35760f77112a5c32b5e67a743322ff242ff09
Sarah Hart Landolt <sarahhartlandolt@Sarahs-MacBook-Pro.local>
2020-03-11 15:49:59 -0500

    commented the code

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -5,6 +5,7 @@ import { useConvictions } from "./ConvictionProvider.js"
     That's your Event Hub. Get a reference to it here.
 */
 const eventHub = document.querySelector(".container")
+// where content is going to --referencing html
 const contentTarget = document.querySelector(".filters__crime")
 
 // On the content target, listen for a "change" event.
@@ -13,22 +14,23 @@ contentTarget.addEventListener("change", changeEvent => {
     // Only do this if the `crimeSelect` element was changed
     if (changeEvent.target.id.startsWith("crimeSelect")) {
         const selectedCrime = changeEvent.target.value 
-        // Create custom event. Provide an appropriate name.
+        // Created custom event called crimeChosenEvent.
         const crimeChosenEvent = new CustomEvent("crimeChosen", {
             detail: {
                 crimeName: selectedCrime
             }
         })
-        // Dispatch to event hub
+        // Dispatch to custom event to event hub
         eventHub.dispatchEvent(crimeChosenEvent)
     }
 })
-
-
+// renders any collection of convictions 
 const render = convictionsCollection => {
-    contentTarget.innerHTML = `
+    contentTarget.innerHTML = 
+    // sets value of Please select crime to zero then maps over the array of crimes and returns an option which renders just a single crime name 
+    `
         <select class="dropdown" id="crimeSelect">
-            <option value="0">Please select a crime...</option>   
+            <option value="0">Please select a crime...</option>  
         ${
             convictionsCollection.map(singleConviction => {
                 return `<option>${singleConviction.name}</option>`
@@ -39,7 +41,8 @@ const render = convictionsCollection => {
     `
 }
 
-
+// exports the function ConvictionSelect 
+// this is the main component which renders all convictions 
 export const ConvictionSelect = () => {
     const convictions = useConvictions()
     render(convictions)
